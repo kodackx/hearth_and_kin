2bd9eb3d89d456cf1d0dd79c7e20c176e3d496d1
Martin Sjöborg <martin@sjoborg.org>
2023-12-03 00:32:02 +0100

    test

--- src/core/database.py
+++ src/core/database.py
@@ -13,7 +13,7 @@ connect_args = {'check_same_thread': False}
 engine = create_engine(sqlite_url, echo=DEBUG, connect_args=connect_args)
 
 
-def create_db_and_tables():
+def create_db_and_tables() -> None:
     SQLModel.metadata.create_all(engine)
 
 
