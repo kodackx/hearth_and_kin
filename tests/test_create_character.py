f6d8cfb6820370248239c19895f07b22da13fb7b
Martin Sjöborg <martin@sjoborg.org>
2024-01-06 22:50:27 +0100

    Remove redundant test

--- tests/test_create_character.py
+++ tests/test_create_character.py
@@ -33,28 +33,6 @@ def test_create_character(session: Session, client: TestClient):
     # assert response.status_code == 400
 
 
-# Test to get a character
-def test_get_character(session: Session, client: TestClient):
-    _ = client.post('/user', json={'password': 'test', 'username': 'test_user'})
-    _ = client.post(
-        '/character',
-        json={
-            'username': 'test_user',
-            'user_description': 'desc',
-            'goal': 'to find the secret',
-            'charisma': 5,
-        },
-    )
-
-    # Try get character
-    response = client.get('/user/test_user/characters')
-    assert response.status_code == 200
-
-    # Try add a cduplicate character
-    # response = client.post('/character', json={'username': 'test_user', 'user_description': 'desc'})
-    # assert response.status_code == 400
-
-
 # Test to update a character
 def test_update_character(session: Session, client: TestClient):
     _ = client.post('/user', json={'password': 'test', 'username': 'test_user'})
