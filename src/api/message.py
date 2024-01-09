6e2b73ca640edca1c5235a73b71560482917b924
Martin Sjöborg <martin@sjoborg.org>
2024-01-09 23:41:40 +0100

    Websocket works!

--- src/api/message.py
+++ src/api/message.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, WebSocket, WebSocketException
+from fastapi import APIRouter, Depends, HTTPException, WebSocket, WebSocketDisconnect, WebSocketException
 from sqlmodel import Session, select
 
 from ..core.config import GENERATE_AUDIO, GENERATE_IMAGE, logger
@@ -6,6 +6,7 @@ from ..core.database import get_session
 from ..models.character import Character
 from ..models.message import Message, MessageBase, MessageRead
 from ..services import audio, imagery, narrator
+import json
 
 router = APIRouter()
 
@@ -32,53 +33,57 @@ manager = ConnectionManager()
 @router.websocket('/ws/prompt')
 async def generate_reply(websocket: WebSocket, session: Session = Depends(get_session)):
     await manager.connect(websocket)
+    try:
+        while True:
+            message = await websocket.receive_json()
+            logger.debug(f'[MESSAGE] {message = }')
+            message: MessageBase = MessageBase.model_validate(message)
+            # TODO: do we have to recreate the chain at every call? Possible to cache this?
+            messages = session.exec(select(Message).where(Message.story_id == message.story_id)).all()
+            chain = narrator.initialize_chain(narrator.prompt, messages)  # type: ignore
+
+            # TODO: move the openai/audio/narrator stuff to a message/orchestrator service instead
+            audio_id = audio_path = image_url = image_path = None
+            try:
+                logger.debug(f'[MESSAGE] {message.message = }')
+                # Not sure if we want to get this from the endpoint or just query the db here
+                character = session.get(Character, message.character_id)
+                if character is None:
+                    raise WebSocketException(404, 'Character not found')
+                # Will send to openai and obtain reply
+                narrator_reply = narrator.gpt_narrator(character=character, message=message, chain=chain)
+
+                if GENERATE_AUDIO:  # Will send to narrator and obtain audio
+                    audio_id, audio_path = audio.obtain_audio(narrator_reply)
+                    _ = await audio.store_audio(audio_id, audio_path)
+                if GENERATE_IMAGE:  # Will send to dalle3 and obtain image
+                    image_url = imagery.generate_image(narrator_reply)
+                    logger.debug(f'[MESSAGE] {image_url = }')
+                    image_path = await imagery.store_image(image_url)
+                    logger.debug(f'[MESSAGE] {image_path = }')
+            except Exception as e:
+                logger.error(f'[MESSAGE] {e}')
+                raise WebSocketException(500, f'An error occurred while generating the response: {e}')
+                #raise HTTPException(500, f'An error occurred while generating the response: {e}')
+
+            new_message = Message(
+                story_id=message.story_id,
+                character_id=message.character_id,
+                username=message.username,
+                message=message.message,
+                narrator_reply=narrator_reply,
+                audio_path=audio_path,
+                image_path=image_path,
+            )
+            session.add(new_message)
+            session.commit()
+            session.refresh(new_message)
+
+            await manager.broadcast(new_message.model_dump(exclude='timestamp'))
     
-    while True:
-        message = await websocket.receive_json()
-        logger.debug(f'[MESSAGE] {message = }')
-        message: MessageBase = MessageBase.model_validate(message)
-        # TODO: do we have to recreate the chain at every call? Possible to cache this?
-        messages = session.exec(select(Message).where(Message.story_id == message.story_id)).all()
-        chain = narrator.initialize_chain(narrator.prompt, messages)  # type: ignore
-
-        # TODO: move the openai/audio/narrator stuff to a message/orchestrator service instead
-        audio_id = audio_path = image_url = image_path = None
-        try:
-            logger.debug(f'[MESSAGE] {message.message = }')
-            # Not sure if we want to get this from the endpoint or just query the db here
-            character = session.get(Character, message.character_id)
-            if character is None:
-                raise WebSocketException(404, 'Character not found')
-            # Will send to openai and obtain reply
-            narrator_reply = narrator.gpt_narrator(character=character, message=message, chain=chain)
-
-            if GENERATE_AUDIO:  # Will send to narrator and obtain audio
-                audio_id, audio_path = audio.obtain_audio(narrator_reply)
-                _ = await audio.store_audio(audio_id, audio_path)
-            if GENERATE_IMAGE:  # Will send to dalle3 and obtain image
-                image_url = imagery.generate_image(narrator_reply)
-                logger.debug(f'[MESSAGE] {image_url = }')
-                image_path = await imagery.store_image(image_url)
-                logger.debug(f'[MESSAGE] {image_path = }')
-        except Exception as e:
-            logger.error(f'[MESSAGE] {e}')
-            raise WebSocketException(500, f'An error occurred while generating the response: {e}')
-            #raise HTTPException(500, f'An error occurred while generating the response: {e}')
-
-        new_message = Message(
-            story_id=message.story_id,
-            character_id=message.character_id,
-            username=message.username,
-            message=message.message,
-            narrator_reply=narrator_reply,
-            audio_path=audio_path,
-            image_path=image_path,
-        )
-        session.add(new_message)
-        session.commit()
-        session.refresh(new_message)
-
-        await manager.broadcast(new_message.model_dump_json())
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast("Client left the chat")
 
 
 @router.post('/message', response_model=MessageRead)
--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# mock envs for test
-
-os.environ['OPENAI_API_KEY'] = 'mock'
-os.environ['ELEVENLABS_API_KEY'] = 'mock'
-os.environ['ELEVENLABS_VOICE_ID'] = 'mock'
-os.environ['MONGODB_URL'] = 'mock'
