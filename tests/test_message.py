67fba85215c0f82876b004905e1a79030705b259
Martin Sjöborg <martin@sjoborg.org>
2024-01-09 21:29:53 +0100

    First draft of message ws

---
+++ tests/test_message.py
@@ -0,0 +1,27 @@
+from fastapi.testclient import TestClient
+from sqlmodel import Session
+from src.models.message import MessageBase, MessageCreate
+
+def test_websocket(session: Session, client: TestClient):
+    _ = client.post('/user', json={'password': 'test', 'username': 'test_user'})
+    _ = client.post('/story', json={'creator': 'test_user', 'story_id': 1})
+    _ = client.post(
+        '/character',
+        json={
+            'username': 'test_user',
+            'user_description': 'desc',
+            'goal': 'to find the secret',
+            'charisma': 5,
+        },
+    )
+
+    with client.websocket_connect("/ws/prompt") as websocket:
+        message = MessageCreate.model_validate({
+            'story_id': 1,
+            'username': 'test_user',
+            'character_id': 1,
+            'message': 'I open my eyes'
+        })
+        websocket.send_json(message.model_dump(exclude='timestamp'))
+        data = websocket.receive_json()
+        assert data == {"msg": "Hello WebSocket"}
