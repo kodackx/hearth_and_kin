e4abe5ca8e501bb412138c2131b6dbf111bd3057
Martin Sjöborg <martin@sjoborg.org>
2024-02-14 23:34:30 +0100

    fix test env

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,8 @@
 import os
 
-os.environ['OPENAI_API_KEY'] = ''
-os.environ['ELEVENLABS_API_KEY'] = ''
-os.environ['ELEVENLABS_VOICE_ID'] = ''
+os.environ['OPENAI_API_KEY'] = 'mock'
+os.environ['ELEVENLABS_API_KEY'] = 'mock'
+os.environ['ELEVENLABS_VOICE_ID'] = 'mock'
 DEBUG = True
 GENERATE_IMAGE = False
 GENERATE_AUDIO = False
