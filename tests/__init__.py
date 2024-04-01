95ce058d353d45f400e95c1f2af72494196a0e2d
Martin Sjöborg <martin@sjoborg.org>
2024-04-01 18:38:16 +0200

    Fix test

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,6 +3,7 @@ import os
 os.environ['OPENAI_API_KEY'] = 'mock'
 os.environ['ELEVENLABS_API_KEY'] = 'mock'
 os.environ['ELEVENLABS_VOICE_ID'] = 'mock'
+os.environ['AZURE_CONNECTION_STRING'] = 'mock'
 DEBUG = True
 GENERATE_IMAGE = False
 GENERATE_AUDIO = False
