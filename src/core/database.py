403bee4a78b07a028bd1ec2873059aede6991fb3
Martin Sjöborg <martin@sjoborg.org>
2024-04-14 17:11:30 +0200

    azure db connection works

--- src/core/database.py
+++ src/core/database.py
@@ -1,4 +1,7 @@
 from sqlmodel import SQLModel, create_engine, Session
+from sqlalchemy import text
+import urllib
+from src.core.storage import get_secret
 
 sqlite_url = 'sqlite:///db/database.db'
 
@@ -14,3 +17,18 @@ def create_db_and_tables() -> None:
 def get_session():
     with Session(engine) as session:
         yield session
+
+username = get_secret('db-test-admin-username')
+password = get_secret('db-test-admin-password')
+params = urllib.parse.quote_plus(r'Driver={ODBC Driver 18 for SQL Server};Server=tcp:hearthandkin-test.database.windows.net,1433;Database=test;Uid=' + username + ';Pwd=' + password + ';Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
+engine_azure = create_engine(conn_str,echo=False, connect_args=connect_args)
+
+
+with engine_azure.connect() as connection:
+    # Run the SELECT 1 query
+    result = connection.execute(text("SELECT 1;"))
+    # Fetch the result
+    row = result.fetchone()
+    # Print the result
+    print(row[0])
