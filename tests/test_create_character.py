15fcf15b9ebda70d6a25bc5f2bbdabaf491ba13b
Martin Sjöborg <martin@sjoborg.org>
2024-06-27 21:05:10 +0200

    Fix create character

--- tests/test_create_character.py
+++ tests/test_create_character.py
@@ -1,68 +1,52 @@
 from fastapi.testclient import TestClient
 from sqlmodel import Session
-from src.models.character import Character
+from src.models.character import Character, CharacterRead
+from tests.test_create_user import create_user
 
+
+default_character_data = {
+    'description': 'desc',
+    'stat_cha': 5,
+}
+
+
+def create_character(client: TestClient, session: Session, character_data: dict | None = None) -> CharacterRead:
+    # Create character
+    response = client.post('/createcharacter', json=character_data or default_character_data)
+    assert response.status_code == 201, 'Character should be created successfully'
+    character = response.json()
+    character = session.get(Character, character['character_id'])
+    assert character is not None
+    return CharacterRead.model_validate(character)
 
 # Test to create a character
-def test_create_character(session: Session, client: TestClient):
-    _ = client.post('/user', json={'password': 'test', 'username': 'test_user'})
-
-    # Try create character
-    response = client.post(
-        '/createcharacter',
-        json={
-            'username': 'test_user',
-            'description': 'desc',
-            'goal': 'to find the secret',
-            'charisma': 5,
-        },
-    )
-
-    # Verify character added
-    assert response.status_code == 201
-    assert 'character_id' in response.json().keys(), response.json()
-    character_id = response.json()['character_id']
-    character = session.get(Character, character_id)
-    assert character is not None
-    # TODO:make this dynamic
-    assert character.charisma == 5
-    assert character.goal == 'to find the secret'
-
-    # Try add a cduplicate character
-    # response = client.post('/character', json={'username': 'test_user', 'user_description': 'desc'})
-    # assert response.status_code == 400
+def test_create_character(session: Session, client: TestClient) -> CharacterRead:
+    character_data = default_character_data
+    # Create a user first
+    user = create_user(client, session)
+    character_data['user_id'] = user.user_id
+    # Create character
+    character = create_character(client, session, character_data)
+    return character
 
 
 # Test to update a character
 def test_update_character(session: Session, client: TestClient):
-    _ = client.post('/user', json={'password': 'test', 'username': 'test_user'})
+    character = test_create_character(session, client)
 
-    # Create character
-    response = client.post(
-        '/createcharacter',
-        json={
-            'username': 'test_user',
-            'description': 'desc',
-            'goal': 'to find the secret',
-            'charisma': 5,
-        },
-    )
-    character_id = response.json()['character_id']
-    # Update character
+    new_character_data = character.model_dump()
+    new_character_data['description'] = 'new desc'
+    new_character_data['stat_cha'] = 1
+
     response = client.patch(
-        f'/character/{character_id}',
-        json={
-            'username': 'test_user',
-            'description': 'new desc',
-            'goal': 'to find the new secret',
-            'charisma': 1,
-        },
+        f'/character/{character.character_id}',
+        json=new_character_data,
     )
 
     # Verify character updated
-    assert response.status_code == 201, response.json()
-    character = session.get(Character, 1)
-    assert character is not None
-    # TODO:make this dynamic
-    assert character.charisma == 1
-    assert character.goal == 'to find the new secret'
+    assert response.status_code == 201, 'Character should be updated successfully'
+    updated_character = session.get(Character, character.character_id)
+    assert updated_character is not None
+    assert updated_character != character, 'Character should be updated with new data'
+    assert updated_character.stat_cha == new_character_data['stat_cha'], 'Character stat_cha should be updated'
+    assert updated_character.description == new_character_data['description'], 'Character description should be updated'
