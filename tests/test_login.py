cbd00007abf14fcd3a89318633b00be3cfe9c406
Martin Sjöborg <martin@sjoborg.org>
2024-06-27 20:29:53 +0200

    Fix login test

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,7 +3,7 @@ from sqlmodel import Session
 from src.models.user import UserBase
 from fastapi.testclient import TestClient
 from pydantic import ValidationError
-
+from tests.test_create_user import create_user, default_user_data
 
 @pytest.mark.parametrize(
     'user_input',
@@ -22,15 +22,14 @@ def test_login_model(user_input):
 @pytest.mark.parametrize(
     'user_input, expected_status',
     [
-        ({'username': 'user', 'password': 'pass'}, 200),
+        (default_user_data, 200),
         ({'username': 'user', 'password': 'wrong_pass'}, 401),
     ],
 )
 @pytest.mark.asyncio
 async def test_login_logic(user_input, expected_status, session: Session, client: TestClient):
     # Create the user first
-    response = client.post('/user', json={'username': 'user', 'password': 'pass'})
-    assert response.status_code == 201
+    _ = create_user(client, session, default_user_data)
     # Try logging in
-    response = client.post('/login', json=user_input)
-    assert response.status_code == expected_status
+    response = client.post('/login', data=user_input)
+    assert response.status_code == expected_status, 'Logging in should return the expected status code'
